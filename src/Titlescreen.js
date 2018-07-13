a5a4389f74ff34d23e287ce15be6ff70bdab9264
gorniczy <denysgornichar@gmail.com>
2018-07-13 19:16:28 +0200

    add Titlescreen react component

---
+++ src/Titlescreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Dialogue from './App';
+import { Button } from './App';
+
+export class Title extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Rect Dating App</h1>
+        <button>Press to m(edit)ate
+        </button>
+      </div>
+    );
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Dialogue from './App';
 import { Button } from './App';
+import { Title } from './Titlescreen';
 import registerServiceWorker from './registerServiceWorker';
 
-const MyApp = () => {
+/* const MyApp = () => {
   return  (<div>
   <Dialogue answer="- Yeah!" />
   <Button />
   </div>);
+}*/
+
+class MyApp extends React.Component {
+
+  render() {
+    return <Title />
+  }
 }
 
 ReactDOM.render(<MyApp />, document.getElementById('root'));
