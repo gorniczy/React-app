6f6625c2a3f31704c8a525793915ba8dbd8be843
gorniczy <denysgornichar@gmail.com>
2018-07-15 11:58:08 +0200

    edit message

--- src/Dialogue.js
+++ src/Dialogue.js
@@ -5,7 +5,7 @@ export class Dialogue extends React.Component {
   render() {
     return (
       <div className="dialogue">
-        <h2>- Wanna dance?
+        <h2>Wanna dance?
         </h2>
       </div>
     );
